b3aa79311a6a357e9edacbdf97d859c4aa77e0eb
Jim Greaves <greavesjim3@gmail.com>
2019-11-18 01:59:10 -0700

    Final for now

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let dirt = {
   count: 0,
-  mulitplier: 1
+  mulitplier: 1,
+  passive: 0
 };
 let countAdd = 0;
 let multiplier = {
@@ -16,30 +17,38 @@ let shovel = {
 let drill = {
   purchased: 0,
   count: 0,
-  cost: 200,
+  cost: 400,
   multiplierAdd: 1.5
 };
 let digger = {
   purchased: 0,
   count: 0,
-  cost: 200,
+  cost: 50,
   passiveAdd: 1
 };
 let driller = {
   purchased: 0,
   count: 0,
-  cost: 6,
+  cost: 75,
   passiveAdd: 5
 };
 let dirtTotal = document.querySelector("#dirt-total");
 let multiplierTotal = document.querySelector("#multiplier-total");
 let clickAdd = document.querySelector("#click-add");
 let passiveTotal = document.querySelector("#passive-add");
+let shovelOnpage = document.querySelector("#shovel");
+let drillerOnpage = document.querySelector("#driller");
+let drillOnpage = document.querySelector("#drill");
+let diggerOnpage = document.querySelector("#digger");
+let shovelCost = document.querySelector("#shovel-cost");
+let drillCost = document.querySelector("#drill-cost");
+let drillerCost = document.querySelector("#driller-cost");
+let diggerCost = document.querySelector("#digger-cost");
 
 function mine() {
   dirt.count++;
   dirt.count += countAdd;
-  dirt.cout * multiplier.count;
+  dirt.count *= multiplier.count;
   drawMine();
 }
 
@@ -47,6 +56,15 @@ function drawMine() {
   dirtTotal.innerText = dirt.count;
   multiplierTotal.innerText = multiplier.count;
   clickAdd.innerText = countAdd;
+  passiveTotal.innerText = dirt.passive;
+  shovelOnpage.innerText = shovel.purchased;
+  drillerOnpage.innerText = driller.purchased;
+  drillOnpage.innerText = drill.purchased;
+  diggerOnpage.innerText = digger.purchased;
+  shovelCost.innerText = shovel.cost;
+  drillCost.innerText = drill.cost;
+  drillerCost.innerText = driller.cost;
+  diggerCost.innerText = digger.cost;
 }
 drawMine();
 
@@ -65,7 +83,7 @@ function purchaseDrill() {
     dirt.count -= drill.cost;
     drill.purchased++;
     multiplier.count++;
-    drill.cost += 4;
+    drill.cost += 100;
   }
   drawMine();
 }
@@ -73,8 +91,12 @@ function purchaseDigger() {
   if (dirt.count >= digger.cost) {
     dirt.count -= digger.cost;
     digger.purchased++;
-    countAdd++;
-    digger.cost += 4;
+    dirt.passive++;
+    digger.cost += 8;
+    setInterval(function() {
+      dirt.count += dirt.passive;
+      drawMine();
+    }, 3000);
   }
   drawMine();
 }
@@ -82,8 +104,16 @@ function purchaseDriller() {
   if (dirt.count >= driller.cost) {
     dirt.count -= driller.cost;
     driller.purchased++;
-    countAdd++;
-    driller.cost += 4;
+    dirt.passive += 2;
+    driller.cost += 10;
+    setInterval(function() {
+      dirt.count += dirt.passive;
+      drawMine();
+    }, 3000);
   }
   drawMine();
+}
+
+function dirtInterval() {
+  dirt.count += dirt.passive;
 }
